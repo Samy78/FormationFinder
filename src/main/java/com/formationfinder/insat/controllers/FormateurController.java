2763faf275cfc562bf0afeccadb1b61accb57e6b
Samy78 <garali.samy@gmail.com>
2017-03-24 22:55:40 +0100

    up

--- src/main/java/com/formationfinder/insat/controllers/FormateurController.java
+++ src/main/java/com/formationfinder/insat/controllers/FormateurController.java
@@ -2,12 +2,20 @@ package com.formationfinder.insat.controllers;
 
 import com.formationfinder.insat.models.Formateur;
 import com.formationfinder.insat.models.FormateurDAO;
+import com.sun.jersey.core.header.FormDataContentDisposition;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.*;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 /**
  * Created by Samy on 26/01/2017.
@@ -16,6 +24,8 @@ import java.util.List;
 public class FormateurController {
     @Autowired
     private FormateurDAO FormateurDAO;
+    //private static final String UPLOAD_FOLDER = "c:/uploadedFiles/";
+// @Consumes(MediaType.MULTIPART_FORM_DATA)
 
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value="/createFormateur", method = RequestMethod.POST,headers="Accept=application/json")
@@ -30,11 +40,33 @@ public class FormateurController {
             String note,
             String dossier,
             String motDePasse,
-            Integer active,
+            String active,
             String cin) {
         Formateur f = null;
         try {
-            f = new Formateur(nom,prenom,dateNaissance,email,telephone,specialite,note,dossier,motDePasse,active,cin);
+
+// check if all form parameters are provided
+            //if (uploadedInputStream == null || fileDetail == null)
+               // return Response.status(400).entity("Invalid form data").build();
+            // create our destination folder, if it not exists
+           /* try {
+                createFolderIfNotExists(UPLOAD_FOLDER);
+            } catch (SecurityException se) {
+                /*return Response.status(500)
+                        .entity("Can not create destination folder on server")
+                        .build();*/
+          /*  }
+            String uploadedFileLocation = UPLOAD_FOLDER + fileDetail.getFileName();
+            try {
+                saveToFile(uploadedInputStream, uploadedFileLocation);
+            } catch (IOException e) {
+               // return Response.status(500).entity("Can not save file").build();
+            }
+          //  return Response.status(200)
+            //        .entity("File saved to " + uploadedFileLocation).build();
+*/
+Integer activee=parseInt(active);
+            f = new Formateur(nom,prenom,dateNaissance,email,telephone,specialite,note,dossier,motDePasse,activee,cin);
             FormateurDAO.save(f);
         }
         catch (Exception ex) {
@@ -42,6 +74,33 @@ public class FormateurController {
         }
         return f;
     }
+  /*  private void saveToFile(InputStream inStream, String target)
+            throws IOException {
+        OutputStream out = null;
+        int read = 0;
+        byte[] bytes = new byte[1024];
+        out = new FileOutputStream(new File(target));
+        while ((read = inStream.read(bytes)) != -1) {
+            out.write(bytes, 0, read);
+        }
+        out.flush();
+        out.close();
+    }
+    /**
+     * Creates a folder to desired location if it not already exists
+     *
+     * @param dirName
+     *            - full path to the folder
+     * @throws SecurityException
+     *             - in case you don't have permission to create the folder
+     */
+  /*  private void createFolderIfNotExists(String dirName)
+            throws SecurityException {
+        File theDir = new File(dirName);
+        if (!theDir.exists()) {
+            theDir.mkdir();
+        }
+    }*/
 
     @RequestMapping(value="/deleteFormateur", method = RequestMethod.GET,headers="Accept=application/json")
     @ResponseBody
